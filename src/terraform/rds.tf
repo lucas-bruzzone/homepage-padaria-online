90bf9f60633af349773c3c5aacab6ce92ce41e56
lucas-bruzzone <lucas.rbruzzone@gmail.com>
2025-09-24 11:18:38 -0300

    feat: set RDS instance to be publicly accessible

--- src/terraform/rds.tf
+++ src/terraform/rds.tf
@@ -66,7 +66,7 @@ resource "aws_db_instance" "padaria_postgres" {
 
   db_subnet_group_name   = aws_db_subnet_group.padaria_db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  publicly_accessible    = false
+  publicly_accessible    = true
 
   backup_retention_period = 0
   skip_final_snapshot     = true
